3cee16923347cbbe717b66f084f5ba4883e8e425
Akash Gupta <akashgupta7363@gmail.com>
2023-05-18 13:17:17 +0530

    changes in landing  Pages

--- client/src/components/landingpage/Header/LandingHeader.jsx
+++ client/src/components/landingpage/Header/LandingHeader.jsx
@@ -1,46 +1,49 @@
-
-import './landingheader.css';
+import "./landingheader.css";
 
 function LandingHeader() {
-    return(
-        <>
-             <div>
-            <nav class="navbar bg-body-tertiary">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">
-                        <img src="/education.png
-                         " alt="Logo" width="50px" height="40px" class="d-inline-block align-text-top"/>
-                        <span className='logo-den'>Den</span> 
-                     </a>
-                     <b>Login</b>
-                </div>
-            </nav>
+  return (
+    <>
+      <div>
+        <nav class="navbar">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+              <img
+                src="/education.png
+                         "
+                alt="Logo"
+                width="50px"
+                height="40px"
+              />
+              <span className="logo-den">Den</span>
+            </a>
+            <b>Login</b>
+          </div>
+        </nav>
 
-                <div className="container" style={{height:"90vh"}}>
-                <div className="row">
-                    <div className="col-sm-12 col-md-6 p-5 main-heading">
-                          <div><span style={{fontSize:"4rem"}}>Hey there</span><br />
-                            Welcome to <br />
-                            Tuitor's <span className="den">Den</span>
-                           </div>
-                           <p>An online platform where Teachers and <br/>students connect...</p>
-                         <button className="register">Register</button>
-                         <button className="about">About Us</button>
-                    </div>
-                   
-                    <div className="col-sm-12 col-md-6 illustration-container">
-                        <img src="/illustration.svg
-                           
-                         "  className="img-fluid  p-5" alt="Logo" />
-                    </div>
-                </div>
-                </div>
-                </div>
-
-              
-            
-        </>
-    );
+        <div className="container" /*style={{ height: "90vh" }}*/>
+          <div className="left">
+            <div className="main-heading">
+              <div>
+                <span style={{ fontSize: "4rem" }}>Hey there</span>
+                <br />
+                Welcome to <br />
+                Tuitor's <span className="den">Den</span>
+              </div>
+              <p>
+                An online platform where Teachers and <br />
+                students connect...
+              </p>
+              <button className="register">Register</button>
+              <button className="about">About Us</button>
+            </div>
+          </div>
+          <div className="right">
+            <img src="/illustration.svg" alt="Logo" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default LandingHeader;

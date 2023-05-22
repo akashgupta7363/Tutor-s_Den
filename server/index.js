c68ae29f5bd474d896e98c517dfde9ba9e358288
Akash Gupta <akashgupta7363@gmail.com>
2023-05-22 13:01:26 +0530

    m

--- server/index.js
+++ server/index.js
@@ -38,3 +38,4 @@ mongoose
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
   )
   .catch((error) => console.log("Mongo Error", error.message));
+mongoose.set("strictQuery", false);

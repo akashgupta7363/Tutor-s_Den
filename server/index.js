259f7a76e8710d714061d28454ae67488439c0a6
Akash Gupta <akashgupta7363@gmail.com>
2023-05-28 14:22:12 +0530

    url axios

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() =>
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+    app.listen(PORT, () => console.log(`Server  is running on port ${PORT}`))
   )
   .catch((error) => console.log("Mongo Error", error.message));
 mongoose.set("strictQuery", false);
